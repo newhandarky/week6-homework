aa9e981bffeb647e6c4e75ef389c1c8fd2a3fd34
newhandarky <84302055+newhandarky@users.noreply.github.com>
2023-08-21 04:10:17 +0800

    新增aos與gsap練習

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 import './assets/scss/all.scss';
 
+// vendor
+// import "aos/dist/aos";
+// import "./js/aos/aos"; // 自己複製一隻js檔案出來
+// import AOS from 'aos';
+
+// import { gsap } from "gsap";
+// import { ScrollTrigger } from "gsap/ScrollTrigger";
+
 import "bootstrap/dist/js/bootstrap";
