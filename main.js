794224b778a82081a142556b757f35095adc1e59
newhandarky <84302055+newhandarky@users.noreply.github.com>
2023-08-21 10:51:48 +0800

    新增登入頁面認證錯誤訊息

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import './assets/scss/all.scss';
 // import AOS from 'aos';
 
 // import { gsap } from "gsap";
-// import { ScrollTrigger } from "gsap/ScrollTrigger";
+// import { ScrollTrigger } from "gsap/ScrollTrigger.js";
+
 
 import "bootstrap/dist/js/bootstrap";
